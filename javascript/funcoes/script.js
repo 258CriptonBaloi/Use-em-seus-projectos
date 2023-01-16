96814e27dca49a115b3a9722c23396a80d7c5919
Cripton Baloi <criptonbaloi9@gmail.com>
2023-01-16 12:07:35 -0300

    funcoes

---
+++ javascript/funcoes/script.js
@@ -0,0 +1,8 @@
+function teste(){ //Funcao sem parametro
+    alert("Usuario ou senha incorrecta");
+}
+
+function somar( a, b){//Funcao com parametros
+    var soma= a + b; 
+    alert("Soma = "+ soma);
+}
